be2af734d31f6fe09e2480e0c355f380bb7a868f
wisper13 <dorian.meketyn@isen.yncrea.fr>
2020-12-05 15:29:36 +0100

    Premier commit

---
+++ src/app/Post.ts
@@ -0,0 +1,12 @@
+import { Thread } from './Thread';
+
+enum Role {
+    admin = 1,
+    modo = 2,
+    membre = 3
+}
+
+export class Post extends Thread {
+    parentid: number;
+    role: Role;
+}
---
+++ src/app/Thread.ts
@@ -0,0 +1,6 @@
+export class Thread {
+    id: number;
+    date: Date;
+    img: string;
+    message: string;
+}
